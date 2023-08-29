9d46c10f71cf76df082336dbfa5e15115b3fa7f8
Oyiborhoro Benedicta <ladyfsquare@yahoo.com>
2023-08-29 21:30:00 +0100

    First_React_Commit

---
+++ efe_c3/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+      <p>I am Efe Oyiborhoro, this is my first React application.</p>
+
+    </div>
+  );
+}
+
+export default App;
