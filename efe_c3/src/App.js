2a5025c6ee4076923ba6fcc060485c23224f0b0b
Oyiborhoro Benedicta <ladyfsquare@yahoo.com>
2023-08-30 05:54:33 +0100

    App_Updated

--- efe_c3/src/App.js
+++ efe_c3/src/App.js
@@ -1,14 +1,15 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import UserPortfolio from './components/UserPortfolio';
+// import UserPortfolio from './components/UserPortfolio';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-       <UserPortfolio/>
+       <Button/>
     </div>
   );
-}
+};
 
 export default App;
